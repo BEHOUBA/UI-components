884f567c413e051d32864d59e583049a7e5356d0
behouba <behouba@gmail.com>
2018-11-28 07:54:35 +0300

    still working on card component

--- Desktop/advertCard/index.js
+++ Desktop/advertCard/index.js
@@ -3,12 +3,10 @@
 function palindromChecker(word) {
     // declaration d'une variable qui va contenir l'inverse du mot 'word'
     var wordReverse = '';
-
     // boucle inversé afin de construire l'inverse du mot 'word'
     for (var i = word.length - 1; i >= 0; i--) {
         wordReverse += word[i];
     }
-
     // on retourne la comparaison du mot d'origin et son inverse
     return wordReverse === word;
 }
