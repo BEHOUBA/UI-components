a3a9772acea46b4e02251e2c252e6de6dcd90ac9
behouba <behouba@gmail.com>
2018-12-01 20:48:32 +0300

    karl made the categories segmentation

---
+++ Desktop/categories.js
@@ -0,0 +1,110 @@
+[
+    {
+        "name": "INFORMATIQUE ET MULTIMEDIA",
+        "subCategories": [
+            "Téléphones",
+            "Tablettes",
+            "Ordinateurs de bureaux",
+            "Ordinateurs portables",
+            "Accessoires Informatiques Et Gadgets",
+            "Jeux Vidéos Et Consoles",
+            "Appareil Photo Et Photos",
+            "Télévision",
+            "Image Et Son"
+
+        ]
+    },
+    {
+        "name": "VEHICULES",
+        "subCategories": [
+            "Voitures",
+            "Motos",
+            "Vélos",
+            "Véhicules Professionnels",
+            "Pièces Et Accessoires"
+        ]
+    },
+    {
+        "name": "IMMOBILIER",
+        "subCategories": [
+            "Appartements",
+            "Maisons Et Villas",
+            "Bureaux",
+            "Magasins Et Commerce",
+            "Terrain",
+            "Autres Immobiliers"
+
+        ]
+    },
+    {
+        "name": "MAISON ET JARDIN",
+        "subCategories": [
+            "Meubles, Décorations",
+            "Electroménager",
+            "Bricolage",
+            "Jardin Et Outils De Bricolage"
+        ]
+    },
+    {
+        "name": "HABILLEMENT ET BIEN-ETRE",
+        "subCategories": [
+            "Vêtements",
+            "Chaussures",
+            "Montres Et Bijoux",
+            "Sacs Et Accessoires",
+            "Vêtements Pour Enfants Et Bébés",
+            "Equipements Pour Enfants Et Bébé",
+            "Produits De Beauté"
+        ]
+    },
+    {
+        "name": "COURS ET LECONS",
+        "subCategories": [
+            "Soutiens Scolaire",
+            "Cours De Langue",
+            "Informatique",
+            "Musique",
+            "Autres Cours"
+        ]
+    },
+    {
+        "name": "EVENEMENTIELS",
+        "subCategories": [
+            "Concerts, Spectacles",
+            "Organisation D’Evènements",
+            "Expositions, Visites",
+            "Autres Evènements",
+            "Casting, Model"
+        ]
+    },
+    {
+        "name": "SERVICES",
+        "subCategories": [
+            "Traduction",
+            "Organisation D’Evènements",
+            "Services Immobiliers",
+            "Informatiques",
+            "Dépannage Auto",
+            "Aide à Domicile",
+            "Associations, Bénévolat",
+            "Livreur, Déménageur"
+        ]
+    },
+    {
+        "name": "LOISIRS ET DIVERTISSEMENTS",
+        "subCategories": [
+            "CD, DVD, Films",
+            "Sports Et Loisirs",
+            "Livres Et Magazines",
+            "Vins Et Gastronomies"
+        ]
+    },
+    {
+        "name": "EMPLOIS",
+        "subCategories": [
+            "Offre D’emploi",
+            "Demande D’Emploi",
+            "Stage"
+        ]
+    }
+]
